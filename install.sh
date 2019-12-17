876707c954cb265f88d430f1d4e2ba65a26047ab
Hunter Read <hunter.read@discorp.com>
2019-12-17 12:52:27 -0800

    install script fixes, readme fixes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl -fsSL https://raw.github.com/hunter-read/powerlevel10k-custom-os-colors/master/os-colors.zsh > ~/.os-colors.zsh
-echo "source ~/.os_colors.zsh" >> ~/.zshrc
+echo "source ~/.os-colors.zsh" >> ~/.zshrc
